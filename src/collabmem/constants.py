501d3c35b69fe2565d1ef0990eecf3c8551774c3
Matthew Ho <matt.seb.ho@gmail.com>
2026-01-02 01:26:00 +0000

    feat: add mini eval

--- src/collabmem/constants.py
+++ src/collabmem/constants.py
@@ -7,3 +7,4 @@ REPO_ROOT = Path(__file__).parent.parent.parent
 LIC_DATA_PATH = REPO_ROOT / "src/lic/data/sharded_instructions_600.json"
 LIC_PROMPT_DIRECTORY = REPO_ROOT / "src/lic/prompts"
 LIC_EVAL_SUBSET_PATH = REPO_ROOT / "src/lic/data/sharded_eval_subset.json"
+LIC_MINI_EVAL_PATH = REPO_ROOT / "src/lic/data/lic_mini_eval.json"
